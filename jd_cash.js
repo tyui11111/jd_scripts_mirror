dadb2a910ef8e4feced0e4e08865d70319e1b6a5
Joinmogul <68455416+Joinmogul@users.noreply.github.com>
2021-01-28 11:49:17 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -28,9 +28,7 @@ let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭�
 let cookiesArr = [], cookie = '', message;
 let helpAuthor = true;
 const randomCount = $.isNode() ? 20 : 5;
-const inviteCodes = [
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`,
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q`
+const inviteCodes = [''
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
